36741c4d489e8e6c2740b72801f38cd63b309ad5
RodrigoJaldin <85416473+RodrigoJaldin@users.noreply.github.com>
2022-12-06 01:25:38 -0400

    prueba

--- config/livewire.php
+++ config/livewire.php
@@ -122,7 +122,7 @@ return [
     |
     */
 
-    'manifest_path' => null,
+    'manifest_path' => '/tmp/storage/livewire-components.php',
 
     /*
     |--------------------------------------------------------------------------
